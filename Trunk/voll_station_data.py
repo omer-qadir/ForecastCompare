54a4a6002779c3a962fea531b1718d0a90b69e69
Omer Qadir <koder.omer@gmail.com>
2016-06-30 16:33:34 -0400

    initial version of the python script for getting observation data from Trondheim voll station
    This script is copied from the owm script and then some initial changes
    are applied. The script doesn't work yet (work in progress)

---
+++ Trunk/voll_station_data.py
@@ -0,0 +1,107 @@
+#Weather forecast from open weather map
+import urllib.request
+import datetime
+from xml.dom import minidom
+from forecast_db_interface import forecast_db_interface
+# https://github.com/pysimplesoap/pysimplesoap
+from pysimplesoap.client import SoapClient
+
+# http://eklima.met.no/metdata/MetDataService?operation=getMetData
+# http://eklima.met.no/eklimapub/servlet/ReportInfo?action=stationinfo&s=68860&la=en&co=US
+# http://sharki.oslo.dnmi.no/eklimapub/servlet/ReportInfo?action=parameterinfo&tab=T_ELEM_OBS&s=68860&la=en&co=US
+# http://eklima.met.no/Help/Stations/toDay/all/en_e68860.html
+url = 'http://eklima.met.no/metdata/MetDataService?invoke=getMetData&timeserietypeID=0&format=&from=&to=&stations=68860&elements=UM%2CPRM%2CRR%2CTAMRR%2CFFM%2CTAN%2CTAX%2CDD18%2CNNM&hours=&months=&username='
+client = SoapClient(location="http://eklima.met.no/metdata/MetDataService")
+response = client._url_to_xml_tree ("http://eklima.met.no/metdata/MetDataService?invoke=getMetData&timeserietypeID=0&format=&from=&to=&stations=68860&elements=UM%2CPRM%2CRR%2CTAMRR%2CFFM%2CTAN%2CTAX%2CDD18%2CNNM&hours=&months=&username=", False, False)
+
+
+#print (response)
+#repr(response)
+
+dom = minidom.parse(response.as_xml())
+weatherElement = dom.getElementsByTagName('weatherElement')
+currentDate = datetime.date.today()
+
+#db = forecast_db_interface('WeatherForecast.db')
+#db.create_table("VOLL")
+
+raw_observations = []
+dated_observations = {}
+dates = []
+
+for node in weatherElement.getElementsByTagName('item'):
+    symbol      = []
+    precip      = []
+    windDir     = []
+    windSpeed   = []
+    temp        = []
+    pressure    = []
+    humidity    = []
+    # date = node.getAttribute('day')
+    currentId   = node.getElementsByTagName('id')
+
+    #print (currentId)
+    
+#    raw_observations.append({
+#        'date'          : date,
+#        'from'          : '',
+#        'to'            : '',
+#        'symbol'        : symbol.getAttribute('name'),
+#        'precipitation' : precip.getAttribute('value'),
+#        'wind_dir'      : windDir.getAttribute('deg'),
+#        'wind_speed'    : windSpeed.getAttribute('mps'),
+#        'temperature'   : '',
+#        'temp_min'      : temp.getAttribute('min'),
+#        'temp_max'      : temp.getAttribute('max'),
+#        'pressure'      : pressure.getAttribute('value'),
+#        'humidity'      : humidity.getAttribute('value')
+#    })
+#    if date in dates:
+#        dated_observations[date].append({
+#            'from'          : '',
+#            'to'            : '',
+#            'symbol'        : symbol.getAttribute('name'),
+#            'precipitation' : precip.getAttribute('value'),
+#            'wind_dir'      : windDir.getAttribute('deg'),
+#            'wind_speed'    : windSpeed.getAttribute('mps'),
+#            'temperature'   : '',
+#            'temp_min'      : temp.getAttribute('min'),
+#            'temp_max'      : temp.getAttribute('max'),
+#            'pressure'      : pressure.getAttribute('value'),
+#            'humidity'      : humidity.getAttribute('value')
+#        })
+#    else:
+#        dates.append(date)
+#        dated_observations[date] = []
+#        dated_observations[date].append({
+#            'from'          : '',
+#            'to'            : '',
+#            'symbol'        : symbol.getAttribute('name'),
+#            'precipitation' : precip.getAttribute('value'),
+#            'wind_dir'      : windDir.getAttribute('deg'),
+#            'wind_speed'    : windSpeed.getAttribute('mps'),
+#            'temperature'   : '',
+#            'temp_min'      : temp.getAttribute('min'),
+#            'temp_max'      : temp.getAttribute('max'),
+#            'pressure'      : pressure.getAttribute('value'),
+#            'humidity'      : humidity.getAttribute('value')
+#        })
+
+# for date in dates:
+    # print (date)
+    # print (dated_observations[date])
+
+#counter = 0
+#for date in dates: 
+#    values =(datetime.date.today(), date, dated_observations[date][0]['symbol'], dated_observations[date][0]['wind_dir'], dated_observations[date][0]['wind_speed'], 
+#            dated_observations[date][0]['temp_min'], dated_observations[date][0]['temp_max'], dated_observations[date][0]['pressure'], 
+#            dated_observations[date][0]['precipitation'], dated_observations[date][0]['humidity'])
+#    db.insert_row("OWM",values)
+#    counter = counter + 1
+#    if counter >= forecast_db_interface.MAX_DAYS_TO_PREDICT:
+#        break
+#
+#db.commit()
+#db.close()
+
+
