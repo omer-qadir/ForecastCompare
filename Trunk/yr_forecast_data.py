36355cf0a5c16be3fab922478e585d24008d307b
Omer <koder.omer@gmail.com>
2016-07-07 15:11:43 +0000

    No functional change - only whitespace removal

--- Trunk/yr_forecast_data.py
+++ Trunk/yr_forecast_data.py
@@ -26,10 +26,10 @@ for node in tabular_forecast.getElementsByTagName('time'):
     windSpeed   = node.getElementsByTagName('windSpeed')[0]
     temp        = node.getElementsByTagName('temperature')[0]
     pressure    = node.getElementsByTagName('pressure')[0]
-    
+
     date,sep,fromTime = node.getAttribute('from').partition('T')
     toTime   = node.getAttribute('to').partition('T')[2]
-    
+
     raw_forecasts.append({
         'date'          : date,
         'from'          : fromTime,
@@ -69,9 +69,9 @@ for node in tabular_forecast.getElementsByTagName('time'):
             'humidity'      : ''
         })
 counter = 0
-for date in dates: 
+for date in dates:
     temp_min = None
-    temp_max = None 
+    temp_max = None
     for items in dated_forecast[date]:
         tmp_val = float(items['temperature'])
         if temp_min == None or temp_min > tmp_val:
@@ -79,7 +79,7 @@ for date in dates:
         if temp_max == None or temp_max < tmp_val:
             temp_max= tmp_val
 
-    values =(datetime.date.today(), date, dated_forecast[date][0]['symbol'], dated_forecast[date][0]['wind_dir'], dated_forecast[date][0]['wind_speed'], 
+    values =(datetime.date.today(), date, dated_forecast[date][0]['symbol'], dated_forecast[date][0]['wind_dir'], dated_forecast[date][0]['wind_speed'],
             temp_min, temp_max, dated_forecast[date][0]['pressure'], dated_forecast[date][0]['precipitation'], dated_forecast[date][0]['humidity'])
     db.insert_row("YR",values)
     counter = counter + 1
@@ -88,5 +88,5 @@ for date in dates:
 
 db.commit()
 db.close()
-    
+
 
