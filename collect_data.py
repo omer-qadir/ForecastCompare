742cd673e624fb2ec6aacdc5008db52c59813082
Omer <koder.omer@gmail.com>
2016-07-17 18:42:27 +0000

    reordering the sequence of data collection
    voll table acts as foreign key for some of the other tables.
    this means that data in the voll table must be present before data is
    collected for the other tables.
    reodering the sequence of commands so that voll related command is
    exectued before others

--- collect_data.py
+++ collect_data.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python2.7
 
+from forecastSrc.voll_station_data import voll_station_data
 from forecastSrc.bbc_forecast_data import bbc_forecast_data
 from forecastSrc.owm_forecast_data import owm_forecast_data
-from forecastSrc.voll_station_data import voll_station_data
 from forecastSrc.yr_forecast_data import yr_forecast_data
 
+voll_station_data()
 bbc_forecast_data()
 owm_forecast_data()
-voll_station_data()
 yr_forecast_data()
